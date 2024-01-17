6e6aadb807eed53dbb3523bed922b5281fa247fc
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-01-17 23:07:46 +0700

    Add files via upload

---
+++ port/port-change.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipinfo.io/ip);
+# // color
+GREENBO="\e[92;1m"
+NC='\e[0m' # penutup
+BGGREEN="\033[42;37m" # hijau
+BGRED="\033[41;37m" # merah
+BGYELLOW="\033[43;37m" # kuning
+BGPURPLE="\033[45;37m" # ungu
+
+clear
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;100;33m    • CHANGE PORT SERVICE •        \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e " [\e[36m•1\e[0m] Change Port Stunnel4"
+echo -e " [\e[36m•2\e[0m] Change Port OpenVPN"
+echo -e " [\e[36m•3\e[0m] Change Port Trojan"
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo ""
+echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo -e   ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; port-ssl ; exit ;;
+2) clear ; port-ovpn ; exit ;;
+3) clear ; port-tr ; exit ;;
+0) clear ; menu ; exit ;;
+x) exit ;;
+*) echo -e "" ; echo "Anda Salah tekan ." ; sleep 1 ; port-change ;;
+esac
