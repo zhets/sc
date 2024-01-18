4b38b750958b514cb98ab95157732bb192b00191
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-01-18 13:37:37 +0700

    Update addtrgo.sh

--- xray/addtrgo.sh
+++ xray/addtrgo.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 clear
 uuid=$(cat /etc/trojan-go/uuid.txt)
-source /var/lib/SIJA/ipvps.conf
-if [[ "$IP" = "" ]]; then
+source /var/lib/xdxl/ipvps.conf
 domain=$(cat /etc/xray/domain)
-else
-domain=$IP
-fi
 trgo="$(cat ~/log-install.txt | grep -w "Trojan Go" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 clear
@@ -18,7 +14,7 @@ echo -e ""
 		if [[ ${user_EXISTS} == '1' ]]; then
             echo -e "======================================"
 			echo ""
-			echo -e "Nama Sudah ada, Tolong Lu Ganti Tod🥱"
+			echo -e "Nama Sudah ada, Silahkan Ganti dengan username lain !!"
 			exit 1
 		fi
 	done
@@ -30,25 +26,17 @@ hariini=`date -d "0 days" +"%Y-%m-%d"`
 echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
 systemctl restart trojan-go.service
 link="trojan-go://${uuid}@isi_bug_disini:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=%2Ftrojango#$user"
-link1="trojan://${uuid}@isi_bug_disini:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=%2Ftrojango#$user"
+link1="trojan://${uuid}@${domain}:2087/?sni=${domain}&type=ws&host=${domain}&path=%2Ftrojan-go#$user"
 clear
 echo -e "===============[ TROJAN GO ]=============="
-echo -e "Remarks    : ${user}" | tee -a /etc/log-create-user.log
-echo -e "IP/Host    : ${MYIP}" | tee -a /etc/log-create-user.log
-echo -e "Address    : ${domain}" | tee -a /etc/log-create-user.log
-echo -e "Port       : ${trgo}" | tee -a /etc/log-create-user.log
-echo -e "Key        : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "Username   : ${user}" | tee -a /etc/log-create-user.log
+echo -e "Domain     : ${domain}" | tee -a /etc/log-create-user.log
+echo -e "Port       : 2087" | tee -a /etc/log-create-user.log
+echo -e "Uuid       : ${uuid}" | tee -a /etc/log-create-user.log
 echo -e "Encryption : none" | tee -a /etc/log-create-user.log
-echo -e "Path       : /trojango" | tee -a /etc/log-create-user.log
-echo -e "Created    : $hariini" | tee -a /etc/log-create-user.log
-echo -e "Expired    : $exp" | tee -a /etc/log-create-user.log
-echo -e "==============="
-echo -e "Link TrGo  		           : ${link}" | tee -a /etc/log-create-user.log
-echo -e "==============="
-echo -e "Link TrGo v2rayNG	: ${link1}" | tee -a /etc/log-create-user.log
-echo -e "==============="
-echo -e ""
-echo "" | tee -a /etc/log-create-user.log
-read -n 1 -s -r -p "Press any key to back on menu"
-
-menu
+echo -e "Path       : /trojan-go" | tee -a /etc/log-create-user.log
+echo -e "Created On : $hariini" | tee -a /etc/log-create-user.log
+echo -e "Expired On : $exp" | tee -a /etc/log-create-user.log
+echo -e "===============" | tee -a /etc/log-create-user.log
+echo -e "Link TrGO : ${link1}" | tee -a /etc/log-create-user.log
+echo -e "===============" | tee -a /etc/log-create-user.log
