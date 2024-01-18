49d43dcfd6a018db4fadbad8b4f07c9888ff4005
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-01-18 13:34:22 +0700

    Update add-ws.sh

--- xray/add-ws.sh
+++ xray/add-ws.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-#dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-#biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
+
 clear
-source /var/lib/SIJA/ipvps.conf
-if [[ "$IP" = "" ]]; then
+source /var/lib/xdxl/ipvps.conf
 domain=$(cat /etc/xray/domain)
-else
-domain=$IP
-fi
 
 tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
@@ -17,17 +11,17 @@ echo -e "===============[ Add Title Account ]=============="
 echo -e ""
 
 		read -rp "Username :  " -e user
-                read -rp "Limit ip :  " -e limit
+                #read -rp "Limit ip :  " -e limit
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
             echo -e "======================================"
 			echo ""
-			echo "Nama sudah ada, tolong Tod Ganti Nama"
+			echo "Nama sudah ada, Silahkan Gunakan Username lain !!!"
 			echo ""
 			read -n 1 -s -r -p "Press any key to back on menu"
-v2ray-menu
+menu-vmess
 		fi
 	done
 
@@ -78,7 +72,7 @@ grpc=`cat<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "grpc",
-      "path": "vmess-grpc",
+      "path": "vmess",
       "type": "none",
       "host": "",
       "tls": "tls"
@@ -93,27 +87,22 @@ vmesslink3="vmess://$(echo $grpc | base64 -w 0)"
 systemctl restart xray > /dev/null 2>&1
 service cron restart > /dev/null 2>&1
 clear
-echo -e "===============[ VMESS ]=============="
-echo -e "Remarks        : ${user}" | tee -a /etc/log-create-user.log
+echo -e "===============[ VMESS ]==============" | tee -a /etc/log-create-user.log
+echo -e "Username       : ${user}" | tee -a /etc/log-create-user.log
 echo -e "Domain         : ${domain}" | tee -a /etc/log-create-user.log
-echo -e "Port TLS       : ${tls}" | tee -a /etc/log-create-user.log
-echo -e "Port none TLS  : ${none}" | tee -a /etc/log-create-user.log
-echo -e "Port  GRPC     : ${tls}" | tee -a /etc/log-create-user.log
-echo -e "id             : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "Port TLS       : 443, 8443" | tee -a /etc/log-create-user.log
+echo -e "Port none TLS  : 80, 8080" | tee -a /etc/log-create-user.log
+echo -e "Port  GRPC     : 443" | tee -a /etc/log-create-user.log
+echo -e "Uuid           : ${uuid}" | tee -a /etc/log-create-user.log
 echo -e "alterId        : 0" | tee -a /etc/log-create-user.log
 echo -e "Security       : auto" | tee -a /etc/log-create-user.log
-echo -e "Network        : ws" | tee -a /etc/log-create-user.log
+echo -e "Network        : WS or GRPC" | tee -a /etc/log-create-user.log
 echo -e "Path           : /vmess" | tee -a /etc/log-create-user.log
-echo -e "ServiceName    : vmess-grpc" | tee -a /etc/log-create-user.log
-echo -e "==============="
-echo -e "Link TLS       : ${vmesslink1}" | tee -a /etc/log-create-user.log
-echo -e "==============="
-echo -e "Link WS       : ${vmesslink2}" | tee -a /etc/log-create-user.log
-echo -e "==============="
+echo -e "ServiceName    : vmess" | tee -a /etc/log-create-user.log
+echo -e "===============" | tee -a /etc/log-create-user.log
+echo -e "Link SSL/TLS   : ${vmesslink1}" | tee -a /etc/log-create-user.log
+echo -e "===============" | tee -a /etc/log-create-user.log
+echo -e "Link none TLS  : ${vmesslink2}" | tee -a /etc/log-create-user.log
+echo -e "===============" | tee -a /etc/log-create-user.log
 echo -e "Link GRPC    : ${vmesslink3}" | tee -a /etc/log-create-user.log
-echo -e "==============="
-echo -e ""
-echo "" | tee -a /etc/log-create-user.log
-read -n 1 -s -r -p "Press any key to back on menu"
-
-menu
+echo -e "===============" | tee -a /etc/log-create-user.log
