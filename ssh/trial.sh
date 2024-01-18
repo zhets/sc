23f736a48a6b048379bba5086a4683b257a01ff1
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-01-18 16:15:57 +0700

    Update trial.sh

--- ssh/trial.sh
+++ ssh/trial.sh
@@ -1,49 +1,48 @@
 #!/bin/bash
 z="\033[1;93m"
 x="\033[0m"
+Domain=$(cat /etc/xray/domain)"
+hostslow=$( cat /etc/xray/dns )
+serverpub=$( cat /etc/slowdns/server.pub )
+function kill_ssh() {
+userdel -f "$user"
+systemctl restart ws-stunnel
+systemctl restart dropbear
+}
 clear
-echo -e "${z}◇━━━━━━━━━━━━━━━━━━━━━◇${x}"
-echo -e "   Set Masa Aktif ( Menit )"
-echo -e "${z}◇━━━━━━━━━━━━━━━━━━━━━◇${x}"
-read -p " Menit : " Anuku
-clear
-#sldomain=`cat /etc/xray/dns`
-#slkey=`cat /etc/slowdns/server.pub`
-domen=`cat /etc/xray/domain`
-Login=Trial-SSH`</dev/urandom tr -dc X-Z0-9 | head -c3`
-hari="1"
-Pass=1
-echo Create Akun: $Login
-echo Setting Password: $Pass
-clear
-useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
-exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
-echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
-clear
-echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
-echo -e "  <=  SSH ACCOUNT =>"       
-echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
-echo -e "Username   : $Login"
-echo -e "Password   : $Pass"        
-#echo -e "CITY         : $(cat /root/.mycity)"
-#echo -e "ISP          : $(cat /root/.myisp)"
-#echo -e "NSDOMAIN     : $(cat /etc/xray/dns)"
-#echo -e "PUBKEY       : $(cat /etc/slowdns/server.pub)"
-#echo -e "Slowdns      : 53, 5300"
-echo -e "Host            : $(cat /etc/xray/domain)"
-echo -e "Port Dropbear   : 109, 69, 143"
-echo -e "Port OpenSSH    : 22"
-echo -e "Port TLS/SSL    : 443"
-echo -e "Port None TLS   : 80"
-echo -e "Port Udp Custom : 1-65535"
-echo -e "Port Udgpw      : 7300"      
-echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
-echo -e "Payload WS => "
-echo -d "GET wss://$(cat /etc/xray/domain)/ HTTP/1.1[crlf]Host: $(cat /etc/xray/domain)[crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
-echo -e "Masa Aktif : $Anuku Menit"
-echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
-echo userdel -f "$Login" | at now +$Anuku minutes &> /dev/null
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${x}"
+echo -e "\e[40;1;37m        Create SSH Account         ${x}"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${x}"
 echo -e ""
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
+read -p "Expired [ menit ] : " expired
+user=trial-`</dev/urandom tr -dc a-z0-9 | head -c5`
+useradd -e `date -d "1 days" +"%Y-%m-%d"` -s /bin/false -M $user
+exp="$(chage -l $user | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $user &> /dev/null
+hari-ini=`date -d "0 days" +"%Y-%m-%d"`
+clear
+echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"                 
+echo -e "   <=  SSH ACCOUNT =>"       
+echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
+echo -e "Username   : $user"
+echo -e "Password   : $Pass"
+#echo -e "Created On : $hari-ini"
+echo -e "Expired On : $expired Minutes"
+echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
+echo -e "CITY          : $(cat /root/.city)"
+echo -e "ISP           : $(cat /root/.isp)"
+echo -e "Host/IP       : ${Domain}"
+echo -e "Host Slowdns  : ${hostslow}"
+echo -e "Pub key       : ${serverpub}"
+echo -e "Port Slowdns  : 53, 5300"
+echo -e "Port Dropbear : 69, 143"
+echo -r "Port OpenSSH  : 22"
+echo -e "Port SSL/TLS  : 443, 8443"
+echo -e "Port None TLS : 80, 8080"
+echo -e "Port SSH UDP  : 1-65535"
+echo -e "Badvpn Udgpw  : 7100 - 7500"      
+echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
+echo -e "Payload WS  => "
+echo -e "GET wss://[host]/ HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
+echo userdel -f "$user" && systemctl restart ws-stunnel && systemctl restart dropbear| at now +$expired minutes &> /dev/null
