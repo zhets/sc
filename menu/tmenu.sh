83823606f94ac8317689522fbfc66b796fa6fa57
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-01-18 18:53:16 +0700

    Update tmenu.sh

--- menu/tmenu.sh
+++ menu/tmenu.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
-#########################
-trgo="$(systemctl show trojan-go.service --no-page)"
-trojan_server=$(systemctl status xray | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
-strgo=$(echo "${trgo}" | grep 'ActiveState=' | cut -f2 -d=)
-# STATUS SERVICE TROJAN
-if [[ $trojan_server == "running" ]]; then 
-   status_virus_trojan=" ${GREEN}Running ${NC}( No Error )${NC}"
+exitsc="\033[0m"
+y="\033[1;93m"
+j="\033[0;33m"
+function lane() {
+echo -e "${y}────────────────────────────────────────────${exitsc}"
+}
+url_izin="https://raw.githubusercontent.com/zhets/izinsc/main/ip"
+ipsaya=$(curl -sS ipv4.icanhazip.com)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+checking_sc() {
+useexp=$(wget -qO- $url_izin | grep $ipsaya | awk '{print $3}')
+if [[ $date_list < $useexp ]]; then
+echo -ne
 else
-   status_virus_trojan="${RED}  Not Running ${NC}  ( Error )${NC}"
+lane
+echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          ${exitsc}"
+lane
+echo -e ""
+echo -e "            \033[0;35mPERMISSION DENIED !${exitsc}"
+echo -e "   ${j}Your VPS${exitsc} $ipsaya ${j}Has been Banned${exitsc}"
+echo -e "     ${j}Buy access permissions for scripts${exitsc}"
+echo -e "             ${j}Contact Admin :${exitsc}"
+echo -e "      \033[0;36mWhatsapp${exitsc} wa.me/6285935195701"
+lane
+exit
 fi
-# Status Service Trojan GO
-if [[ $strgo == "active" ]]; then
-  status_trgo=" ${GREEN}Running ${NC}( No Error )${NC}"
-else
-  status_trgo="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-#uwew guys,
-trx=$(grep -c -E "^#! " "/etc/xray/config.json")
-let tra=$trx/2
-ssx=$(grep -c -E "### " "/etc/trojan-go/akun.conf")
-let ssa=$ssx/2
-# Color Validation
+}
+checking_sc
 DF='\e[39m'
 Bold='\e[1m'
 Blink='\e[5m'
@@ -38,51 +45,7 @@ GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 LIGHT='\033[0;37m'
 # VPS Information
-#Domain
-domain=$(cat /etc/xray/domain)
-#Status certificate
-modifyTime=$(stat $HOME/.acme.sh/${domain}_ecc/${domain}.key | sed -n '7,6p' | awk '{print $2" "$3" "$4" "$5}')
-modifyTime1=$(date +%s -d "${modifyTime}")
-currentTime=$(date +%s)
-stampDiff=$(expr ${currentTime} - ${modifyTime1})
-days=$(expr ${stampDiff} / 86400)
-remainingDays=$(expr 90 - ${days})
-tlsStatus=${remainingDays}
-if [[ ${remainingDays} -le 0 ]]; then
-	tlsStatus="expired"
-fi
-# OS Uptime
-uptime="$(uptime -p | cut -d " " -f 2-10)"
-# Download
-#Download/Upload today
-dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
-utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
-ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
-#Download/Upload yesterday
-dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
-uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
-tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
-#Download/Upload current month
-dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
-umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
-tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
-# Getting CPU Information
-cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
-cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
-cpu_usage+=" %"
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
-WKT=$(curl -s ipinfo.io/timezone )
-DAY=$(date +%A)
-DATE=$(date +%m/%d/%Y)
-IPVPS=$(curl -s ipinfo.io/ip )
-cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
-cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
-tram=$( free -m | awk 'NR==2 {print $2}' )
-uram=$( free -m | awk 'NR==2 {print $3}' )
-fram=$( free -m | awk 'NR==2 {print $4}' )
-clear
+
 clear
 echo -e "${BIYellow}┌─────────────────────────────────────────────────┐${NC}"
 echo -e "${BIYellow}│ ${BIB}                  • TROJAN •                    ${NC} ${BIYellow}│${NC}"
