4968a2fe6e295821f1d01e0a4e83472d199b47c0
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-01-18 16:10:42 +0700

    Update usernew.sh

--- ssh/usernew.sh
+++ ssh/usernew.sh
@@ -3,44 +3,41 @@ z="\033[1;93m"
 x="\033[0m"
 clear
 Domain=$(cat /etc/xray/domain)"
-IPVPS=(curl ifconfig.me)"
-IP=$(curl -sS ifconfig.me);
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[40;1;37m        Create SSH Account         \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+hostslow=$( cat /etc/xray/dns )
+serverpub=$( cat /etc/slowdns/server.pub )
+
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${x}"
+echo -e "\e[40;1;37m        Create SSH Account         ${x}"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${x}"
 read -p "Username       : " Login
 read -p "Password       : " Pass
-#read -p "Max IP         : " iplimit
-read -p "Expired (Days) : " masaaktif
-sleep 1
-clear
-useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+read -p "Expired (Days) : " expired
+useradd -e `date -d "$expired days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+hari-ini=`date -d "0 days" +"%Y-%m-%d"`
 clear
 echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"                 
 echo -e "   <=  SSH ACCOUNT =>"       
-echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}
+echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
 echo -e "Username   : $Login"
-echo -e "Password   : $Pass"        
-#echo -e "CITY         : $(cat /root/.mycity)"
-#echo -e "ISP          : $(cat /root/.myisp)"
-#echo -e "NSDOMAIN     : $(cat /etc/xray/dns)"
-#echo -e "PUBKEY       : $(cat /etc/slowdns/server.pub)"
-#echo -e "Slowdns      : 53, 5300"
-echo -e "Host            : ${Domain}"
-echo -e "IP              : ${IPVPS}
-echo -e "Port Dropbear   : 109, 69, 143"
-echo -r "Port OpenSSH    : 22"
-echo -e "Port SSL/TLS    : 443"
-echo -e "Port None TLS   : 80"
-echo -e "Port Udp Custom : 1-65535"
-echo -e "Port Udgpw SSH  : 7300"      
+echo -e "Password   : $Pass"
+echo -e "Created On : $hari-ini"
+echo -e "Expired On : $exp"
+echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
+echo -e "CITY          : $(cat /root/.city)"
+echo -e "ISP           : $(cat /root/.isp)"
+echo -e "Host/IP       : ${Domain}"
+echo -e "Host Slowdns  : ${hostslow}"
+echo -e "Pub key       : ${serverpub}"
+echo -e "Port Slowdns  : 53, 5300"
+echo -e "Port Dropbear : 69, 143"
+echo -r "Port OpenSSH  : 22"
+echo -e "Port SSL/TLS  : 443, 8443"
+echo -e "Port None TLS : 80, 8080"
+echo -e "Port SSH UDP  : 1-65535"
+echo -e "Badvpn Udgpw  : 7100 - 7500"      
 echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
 echo -e "Payload WS  => "
 echo -e "GET wss://[host]/ HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf]"
 echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
-echo -e "Masa Aktif : $exp"
-echo -e "${z}◇━━━━━━━━━━━━━━━━━━━◇${x}"
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
