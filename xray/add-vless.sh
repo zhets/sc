c4e5f0378e1811e2b1d5e55ca93ed07146eb7768
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-01-18 13:31:51 +0700

    Update add-vless.sh

--- xray/add-vless.sh
+++ xray/add-vless.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-#dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-#biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
 clear
-source /var/lib/SIJA/ipvps.conf
+source /var/lib/xdxl/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/xray/domain)
 else
@@ -16,17 +13,17 @@ echo -e "===============[ Add Title Account ]=============="
 echo ""
 
 		read -rp "Username: " -e user
-                read -rp "Limit ip: " -e limit
+                #read -rp "Limit ip: " -e limit
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
 		echo -e "======================================"
 			echo ""
-			echo "Nama sudah ada, tolong Dong Tod Lu ganti Nama"
+			echo "Nama sudah ada, Silahkan Gunakan Username lain !!!"
 			echo ""
 			read -n 1 -s -r -p "Press any key to back on menu"
-			v2ray-menu
+			menu-vless
 		fi
 	done
 
@@ -42,29 +39,23 @@ vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#
 vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
 systemctl restart xray
 clear
-echo -e "===============[ VLESS ]=============="
-echo -e ""
-echo -e "Remarks        : ${user}" | tee -a /etc/log-create-user.log
-echo -e "Expiry In      : ${exp}"
-echo -e "Limit Ip       : ${limit}"
+echo -e "===============[ VLESS ]==============" | tee -a /etc/log-create-user.log
+echo -e "" | tee -a /etc/log-create-user.log
+echo -e "Username       : ${user}" | tee -a /etc/log-create-user.log
+echo -e "Expired On     : ${exp}" | tee -a /etc/log-create-user.log
 echo -e "Domain         : ${domain}" | tee -a /etc/log-create-user.log
-echo -e "port TLS       : $tls" | tee -a /etc/log-create-user.log
-echo -e "port none TLS  : $none" | tee -a /etc/log-create-user.log
-echo -e "id             : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "Port SSL/TLS   : 443, 8443" | tee -a /etc/log-create-user.log
+echo -e "Port none TLS  : 80, 8080" | tee -a /etc/log-create-user.log
+echo -e "Uuid           : ${uuid}" | tee -a /etc/log-create-user.log
 echo -e "Encryption     : none" | tee -a /etc/log-create-user.log
 echo -e "Network        : ws" | tee -a /etc/log-create-user.log
 echo -e "Path           : /vless" | tee -a /etc/log-create-user.log
-echo -e "Path           : vless-grpc" | tee -a /etc/log-create-user.log
-echo -e ""
-echo -e "==============="
+echo -e "Servicename    : vless" | tee -a /etc/log-create-user.log
+echo -e "" | tee -a /etc/log-create-user.log
+echo -e "===============" | tee -a /etc/log-create-user.log
 echo -e "Link TLS       : ${vlesslink1}" | tee -a /etc/log-create-user.log
 echo -e "==============="
 echo -e "Link none TLS  : ${vlesslink2}" | tee -a /etc/log-create-user.log
-echo -e "==============="
+echo -e "===============" | tee -a /etc/log-create-user.log
 echo -e "Link GRPC      : ${vlesslink3}" | tee -a /etc/log-create-user.log
-echo -e "==============="
-echo -e ""
-echo "" | tee -a /etc/log-create-user.log
-read -n 1 -s -r -p "Press any key to back on menu"
-
-menu
+echo -e "===============" | tee -a /etc/log-create-user.log
